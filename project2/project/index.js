1183593b74949253f131c7b7814639d86a913de5
unknown <Rishabh Verma>
2019-09-07 16:41:19 +0530

    deployment

--- project2/project/index.js
+++ project2/project/index.js
@@ -1,10 +1,37 @@
 var express = require('express');
+// var multerCloudinary = require('multer-cloudinary');
+// var cloudinary = require('cloudinary').v2;
+// var cloudinaryStorage = multerCloudinary({cloudinary: cloudinary});
 
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+var mongoClient=require("mongodb").MongoClient;
+var db;
+var url="mongodb+srv://RishabhVerma:Rishabh123@mmengine-vmo0p.mongodb.net/?retryWrites=true&w=majority";
+mongoClient.connect(url,{useNewUrlParser:true},function(error,client){
+         if (error)
+            throw error;
+        db=client.db("blog");
+
+
+})
+//  app.get("/blog",function(req,res){
+
+//    db.collection('users').find({}).toArray(function(err,result){
+// //             if(err)
+// //              throw err;
+            
+// //             console.log(result)
+                   
+   
+   
+// //                 res.json(JSON.stringify(result)) 
+// })
+//  });
+
 
 
 app.use(express.urlencoded({ extended:false}));
@@ -17,8 +44,15 @@ app.get("/", function(req, res){
     });
 
 });
+app.post("/Upload", function(req, res){
+    res.render("home" , {
+        style: "/style.css",
+        script:"/data.js"
+    });
+
+});
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Your App is running at port 3000");
 });
--- project2/project/public/data.js
+++ project2/project/public/data.js
@@ -22,6 +22,27 @@ $(document).ready(function(){
                                     }
            })
         
-        
+          //  $.ajax({
+          //   url:'/blog',
+          //   type:'GET',
+          //   success:function(data){
+          //      // console.log(data);
+          //     // window.location.hash = 'Home';
+          //      data=JSON.parse(data);
+          //     //  $('#crd').append("<div class='card-deck' id='dec' style='width:100%;'></div>")
+                  
+          //        for(var i=0;i<data.length;i++)
+          //          {  // data=JSON.stringify(data);
+                   
+          //                //  { $("#deck").append("<div style='display:block;'></div>")}
+          //              //  $('#dec').append('<div class="col-4" style="width:100%;"></div>')
+          //              $("#crd").append("<div class='col-4' style='width:100%;margin-bottom:1%;'><div class='card' style='width: 100%;'><div style='text-align:center;background-color:yellow;'> "+data[i].category+"</div> <img src="+data[i].url+" class='card-img-top' alt='...' width='"+data[i].width+"px' height='"+data[i].height+"px'><div class='card-body' style='height:100%;width:100%;'><h5 class='card-title'>"+data[i].name+"</h5><p class='card-text' style='width:100%;'><button  class='btn btn-primary'>Upvote</button>...<a href='#' class='btn btn-primary'>Comment</a></p></div><div class='card-footer'><small class='text-muted'>SignUp/Login to comment and Upvote</small></div></div></div>")
+                       
+
+          //          }
+
+          //                          }
+          // })
+       
            
           })
