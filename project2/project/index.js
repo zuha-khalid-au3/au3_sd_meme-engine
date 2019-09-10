c942267881bb233f9d16ee9f553879c9f21de666
unknown <Rishabh Verma>
2019-09-11 02:38:26 +0530

    changesRequiredDone

--- project2/project/index.js
+++ project2/project/index.js
@@ -1,4 +1,17 @@
+  
 var express = require('express');
+var bodyParser=require("body-parser");
+var multer=require("multer");
+var upload=multer({dest:'Uploads/Images'});
+const dotenv=require("dotenv");
+dotenv.config();
+var cloudinary=require("cloudinary").v2;
+cloudinary.config({
+    cloud_name:''+process.env.CLOUD_NAME,
+    api_key:''+process.env.API_KEY,
+    api_secret:''+process.env.API_SECRET
+
+})
 // var multerCloudinary = require('multer-cloudinary');
 // var cloudinary = require('cloudinary').v2;
 // var cloudinaryStorage = multerCloudinary({cloudinary: cloudinary});
@@ -10,31 +23,37 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 var mongoClient=require("mongodb").MongoClient;
 var db;
-var url="mongodb+srv://RishabhVerma:Rishabh123@mmengine-vmo0p.mongodb.net/?retryWrites=true&w=majority";
-mongoClient.connect(url,{useNewUrlParser:true},function(error,client){
+var url;
+if(process.env.DB_URL)
+   {url=process.env.DB_URL;
+    console.log(url)}
+else
+   {url="mongodb://127.0.0.1:27017";}
+mongoClient.connect(url,{ useUnifiedTopology: true },function(error,client){
          if (error)
             throw error;
         db=client.db("blog");
 
-
+        console.log("connecting.")
 })
-//  app.get("/blog",function(req,res){
 
-//    db.collection('users').find({}).toArray(function(err,result){
-// //             if(err)
-// //              throw err;
+ app.get("/blog",function(req,res){
+
+   db.collection('users').find({}).toArray(function(err,result){
+            if(err)
+             throw err;
             
-// //             console.log(result)
+          //  console.log(result)
                    
    
    
-// //                 res.json(JSON.stringify(result)) 
-// })
-//  });
+              res.json(result) 
+})
+ });
 
 
 
-app.use(express.urlencoded({ extended:false}));
+app.use(express.urlencoded({ extended:true}));
 
 app.set('view engine', 'hbs');
 app.get("/", function(req, res){
@@ -44,11 +63,26 @@ app.get("/", function(req, res){
     });
 
 });
-app.post("/Upload", function(req, res){
-    res.render("home" , {
-        style: "/style.css",
-        script:"/data.js"
-    });
+app.post("/Upload",upload.single("memes"), function(req, res){
+   // console.log(req.file);
+           cloudinary.uploader.upload(req.file.path,function(error,result){
+           // console.log(error);
+            if (error)
+               throw error;
+
+               //    console.log("result of cloudinary",result);
+
+                   res.render("home.hbs" , {
+                    style: "/style.css",
+                    script:"/data.js"
+                });
+
+
+           })
+
+
+
+    
 
 });
 
